8e13ea1a96a0c0dadb80191762bea9ef111e303d
thanhson391999 <55115530+thanhson391999@users.noreply.github.com>
2020-08-17 17:49:35 +0700

    My Project (day 2)

---
+++ TeduShop/TeduShop.Model/Models/Tag.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace TeduShop.Model.Models
+{
+    [Table("Tags")]
+    public class Tag
+    {
+        [Key]
+        [MaxLength(50)]
+        public string ID { get; set; }
+
+        [Required]
+        [MaxLength(50)]
+        public string Name { get; set; }
+
+        [Required]
+        [MaxLength(50)]
+        public string Type { get; set; }
+
+        public virtual IEnumerable<PostTag> PostTags { get; set; }
+        public virtual IEnumerable<ProductTag> ProductTags { get; set; }
+
+    }
+}
